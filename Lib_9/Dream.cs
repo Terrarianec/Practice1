8d54b580c751372994ccf35bbe42ed6a21cefaad
Terrarianec <dimaryazantcev@gmail.com>
2023-09-30 23:01:10 +0300

    Загрузка всего и сразу

---
+++ Lib_9/Dream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lib_9
+{
+    public class Dream
+    {
+        private static Random _random = new Random(0); // Немного предсказуемости
+
+        /// <summary>
+        /// Формирует n целых чисел в диапазоне 2-14 и находит сумму чисел меньше 8
+        /// </summary>
+        /// <param name="n">Количество сгенерированных чисел</param>
+        /// <param name="numbers">Все сгенерированные числа</param>
+        /// <returns>Сумма сгенерированных n чисел меньше 8</returns>
+        public static int GetSumOfNIntegers(int n,  List<int> numbers)
+        {
+            int sum = 0;
+
+            while (n --> 0)
+            {
+                int num = _random.Next(2, 15);
+
+                if (num < 8)
+                {
+                    sum += num;
+                }
+
+                numbers.Add(num);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Practice1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Lib_9;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace Practice1
+{
+    /// <summary>
+    /// Логика взаимодействия MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(NTextBox.Text, out int n) || n <= 0)
+            {
+                MessageBox.Show("Некорректное значение");
+                return;
+            }
+
+            if (n >= 1e6 &&
+                MessageBox.Show("Расчёт может занять много времени, продолжить?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                return;
+
+            List<int> numbers = new List<int>();
+
+            ResultTextBox.Text = Dream.GetSumOfNIntegers(n, numbers).ToString();
+            NumbersTextBox.Text = string.Join(", ", numbers);
+        }
+
+        private void AboutButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "ИСП-31 Рязанцев Дмитрий Александрович\n" +
+                "Задание: Сформировать n целых чисел в диапазоне 2-14. Найти сумму чисел < 8. Результат вывести на экран",
+                "О программе",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+                );
+        }
+
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы уверены, что хотите выйти?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                Close();
+        }
+
+        private void NTextBoxChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            NumbersTextBox.Text = string.Empty;
+            ResultTextBox.Text = string.Empty;
+        }
+    }
+}
